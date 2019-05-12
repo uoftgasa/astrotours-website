3877e3f514529805300b764b4b450b31f52897f8
Jason Leung <leung@astro.utoronto.ca>
2019-05-12 00:55:07 -0400

    Add building info to JSON file
    Since the tours don't always take place in the same room or building, it's
    better to have the building info in the JSON file for quick changes.

--- js/tours-next.js
+++ js/tours-next.js
@@ -3,6 +3,10 @@ $.getJSON("js/tours-next.json", function(data) {
     $('.talk-speaker').text(data['speaker']);
     $('.talk-date').text(data['month'] + " " + data['day'] + ", " + data['year']);
     $('.talk-time').text(data['time'] + " " + data['timezone']);
+    $('.talk-building').text(data['buildingName']);
+    $('.talk-building-code').text(data['buildingCode']);
+    $('.talk-address').text(data['address']);
+    $('.talk-room').text(data['roomNumber']);
     $('.talk-abstract').text(data['abstract']);
     $('.talk-speaker-info').text(data['speakerInfo']);
 });
