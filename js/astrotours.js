ad15efef32a89377c39b4d480519d2f0eed562ea
Jason Leung <leung@astro.utoronto.ca>
2019-07-01 20:37:38 -0400

    Transfer all sections to collapsibles
    This includes not only the contents, but also required rewiring the navigation
    buttons. A new script was written to handle active/disabled states in the
    navigation bar. Also modified CSS to reflect changes in classes and IDs.

---
+++ js/astrotours.js
@@ -0,0 +1,31 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Handle navigation button states
+  $('.nav-link').click(function() {
+    $('.nav-link').removeClass("disabled active");
+    $(this).addClass("disabled active");
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('#home-btn').click(function() {
+    $('.nav-link').removeClass("disabled active");
+    $('#home-nav').addClass("disabled active");
+  });
+
+  $('#next-tour-btn').click(function() {
+    $('.nav-link').removeClass("disabled active");
+    $('#next-tour-nav').addClass("disabled active");
+  });
+
+  $('#faq-btn').click(function() {
+    $('.nav-link').removeClass("disabled active");
+    $('#faq-nav').addClass("disabled active");
+  });
+
+  $('#location-btn').click(function() {
+    $('.nav-link').removeClass("disabled active");
+    $('#location-nav').addClass("disabled active");
+  });
+
+})(jQuery); // End of use strict
